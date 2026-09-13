f870a8f74115f2b44e81e92aa94f52d1278e2f15
NicolineNV <nnvisby01@gmail.com>
2026-09-13 20:25:59 +0200

    Updated:
    EntityRegistry.java - registered all 14 entities by .addAnnotatedClass()

--- src/main/java/app/config/EntityRegistry.java
+++ src/main/java/app/config/EntityRegistry.java
@@ -1,5 +1,6 @@
 package app.config;
 
+import app.entities.*;
 import org.hibernate.cfg.Configuration;
 
 final class EntityRegistry {
@@ -7,6 +8,29 @@ final class EntityRegistry {
     private EntityRegistry() {}
 
     static void registerEntities(Configuration configuration) {
+
+        // Independent entities
+        configuration.addAnnotatedClass(FlavorTag.class);
+        configuration.addAnnotatedClass(Garnish.class);
+        configuration.addAnnotatedClass(CocktailFamily.class);
+        configuration.addAnnotatedClass(User.class);
+
+        // Ingredient
+        configuration.addAnnotatedClass(Spirit.class);
+        configuration.addAnnotatedClass(Liqueur.class);
+        configuration.addAnnotatedClass(Mixer.class);
+        configuration.addAnnotatedClass(Syrup.class);
+
+        // Join-entities (ingredient <-> flavor_tag)
+        configuration.addAnnotatedClass(SpiritFlavorTag.class);
+        configuration.addAnnotatedClass(LiqueurFlavorTag.class);
+        configuration.addAnnotatedClass(MixerFlavorTag.class);
+        configuration.addAnnotatedClass(SyrupFlavorTag.class);
+
+        // Cocktail and user-relations
+        configuration.addAnnotatedClass(Cocktail.class);
+        configuration.addAnnotatedClass(CocktailMenu.class);
+
 
     }
 }
