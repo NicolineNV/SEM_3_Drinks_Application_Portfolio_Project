518fbf341fd16981c8388aa7c12bdcf5ae521cf8
NicolineNV <nnvisby01@gmail.com>
2026-09-25 11:51:04 +0200

    UPDATES/FIXES/ADDED:
    UPDATES/FIXES:
    
    CocktailDAO.java - made method getByIdWithDetails(Long id) - makes a EntityManager and jpql quary where all fields in cocktail are LEFT JOIN FETCH because can be nullable (To not get NullPointerException)
    
    CocktailMenuDAO.java - updated jpql quary in getFavoritesByUser(Long userId) - JOIN FETCH on Cocktail because will always have at least one cocktail - LEFT JOIN FETCH on all nullable fields (To not get NullPointerException)
    
    logback.xml - updated so that all logs are saved in a log file
    
    AbstractDAO.java - added logger.error in executeInTransaction() method, to log if transaction fails
    
    HibernateEmfBuilder.java - added logger.error in build() method, to log if SessionFactory fails
    
    ADDED:
    
    request.http - to better test future endpoints

--- src/main/java/app/dao/CocktailDAO.java
+++ src/main/java/app/dao/CocktailDAO.java
@@ -1,10 +1,30 @@
 package app.dao;
 
 import app.entities.Cocktail;
+import jakarta.persistence.EntityManager;
+
+import java.util.Optional;
 
 public class CocktailDAO extends AbstractDAO<Cocktail, Long> {
 
     public CocktailDAO(){
         super(Cocktail.class);
+    }
+
+    public Optional<Cocktail> getByIdWithDetails(Long id) {
+        try (EntityManager em = emf.createEntityManager()) {
+            String jpql = "SELECT c FROM Cocktail c " +
+                    "LEFT JOIN FETCH c.cocktailFamily " + // LEFT JOIN FETCH because can be null
+                    "LEFT JOIN FETCH c.spirit " +
+                    "LEFT JOIN FETCH c.liqueur " +
+                    "LEFT JOIN FETCH c.mixer " +
+                    "LEFT JOIN FETCH c.syrup " +
+                    "LEFT JOIN FETCH c.garnish " +
+                    "WHERE c.id = :id";
+            return em.createQuery(jpql, Cocktail.class)
+                    .setParameter("id", id)
+                    .getResultStream()
+                    .findFirst();
+        }
     }
 }
